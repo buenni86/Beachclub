43e4bfcfb6a44d97b15296f16d73805f30b624fc
buenni86 <12584248+buenni86@users.noreply.github.com>
2022-10-20 15:03:03 +0200

    Revert "Update skript.js"
    This reverts commit 5bc18d349e9c57ee5571a5022adf626cc62efca6.

--- skript.js
+++ skript.js
@@ -1,6 +1,4 @@
-import { bootstrapExtra } from "@workadventure/scripting-api-extra";
-
-bootstrapExtra();
+import { } from "https://unpkg.com/@workadventure/scripting-api-extra@^1";
 
 var currentPopup = undefined;
 var isCoWebSiteOpened =  false;
